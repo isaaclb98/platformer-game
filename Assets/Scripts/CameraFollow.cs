60c16d52a48fe6f21a7b68aaf480d1e9b87fb4bb
Isaac <86988576+isaaclb98@users.noreply.github.com>
2024-11-22 23:35:53 -0500

    Better character movement

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,11 +7,14 @@ public class CameraFollow : MonoBehaviour
     public Transform player; 
     public Vector3 offset;  
     public float smoothSpeed;
+    public float verticalFollowSpeed = 2f;
 
     void LateUpdate()
     {
         if (player)
         {
+            float targetY = Mathf.Lerp(transform.position.y, player.position.y + offset.y, Time.deltaTime * verticalFollowSpeed);
+
             Vector3 targetPosition = new Vector3(player.position.x + offset.x, player.position.y + offset.y, transform.position.z);
 
             // Smoothly interpolate between the camera's current position and the target position
